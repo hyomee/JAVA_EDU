c6cd917582a647e50260cd25201844d8efab473a
hyosang <hyomee@naver.com>
2022-02-01 03:01:16 +0900

    산술연산자

---
+++ Operator/src/com/hyomee/operator/ArithmeticOperator.java
@@ -0,0 +1,23 @@
+package com.hyomee.operator;
+
+public class ArithmeticOperator {
+   public static void main(String[] args) {
+      int numA = 10;
+      int numB = 3;
+
+      System.out.println("numA + numB = " + (numA + numB) );
+      System.out.println("numA - numB = " + (numA - numB) );
+      System.out.println("numA * numB = " + (numA * numB) );
+      System.out.println("numA / numB = " + (numA / numB) );
+      System.out.println("numA % numB = " + (numA % numB) );
+
+      double doubleA = 10.27;
+      double doubleB = 3.5;
+
+      System.out.println("doubleA + doubleB = " + (doubleA + doubleB) );
+      System.out.println("doubleA - doubleB = " + (doubleA - doubleB) );
+      System.out.println("doubleA * doubleB = " + (doubleA * doubleB) );
+      System.out.println("doubleA / doubleB = " + (doubleA / doubleB) );
+      System.out.println("doubleA % doubleB = " + (doubleA % doubleB) );
+   }
+}
