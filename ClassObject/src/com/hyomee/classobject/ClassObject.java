3e8b13bf7a40cc20bae46e025eaffaf6ce73a56c
hyosang <hyomee@naver.com>
2022-02-02 18:39:51 +0900

    수정

---
+++ ClassObject/src/com/hyomee/classobject/ClassObject.java
@@ -0,0 +1,7 @@
+package com.hyomee.classobject;
+
+public class ClassObject {
+   public static void main(String[] args) {
+      ClassStruct classStruct = new ClassObject(10);
+   }
+}
---
+++ ClassObject/src/com/hyomee/classobject/ClassStruct.java
@@ -0,0 +1,20 @@
+package com.hyomee.classobject;
+
+
+public class ClassStruct {
+
+   // 필드
+   private int num;
+
+   // 생성자
+   public ClassStruct(int num) {
+      this.num = num;
+   }
+
+   // 메서드
+   public int getNum() {
+      return num;
+   }
+
+
+}
