89cd8ed96cf97984cea972bd91001daec66509cc
hyosang <hyomee@naver.com>
2022-02-05 22:40:09 +0900

    Generic Method

--- genericMethod/src/com/hyomee/genericMethod/GenericMethodMain.java
+++ genericMethod/src/com/hyomee/genericMethod/GenericMethodMain.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 public class GenericMethodMain {
   public static void main(String... args) {
-    List list = new ArrayList();
+    List<String> list = new ArrayList<>();
     list.add("홍길동");
     list.add("홍당무");
     list.add("홍사과");
 
     for (int i = 0; i < list.size(); i++) {
-      String str = (String) list.get(0);
+      String str = list.get(0);
       System.out.println(str);
     }
   }
