e96be78d14daa2ceb6b79ab6ce7c8c8923ba8c0c
hyosang <hyomee@naver.com>
2022-02-05 22:01:49 +0900

    Genetic

--- Generic/src/com/hyomee/generic/GenericMain.java
+++ Generic/src/com/hyomee/generic/GenericMain.java
@@ -8,6 +8,7 @@ public class GenericMain {
     List list = new ArrayList();
     list.add("홍길동");
     list.add("홍당무");
+    list.add("홍사과");
 
     for (int i = 0; i < list.size(); i++) {
       String str = list.get(0);
