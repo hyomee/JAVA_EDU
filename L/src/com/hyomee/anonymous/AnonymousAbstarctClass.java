92874a851e740655c56e963bf0e8295ae0781e9e
hyosang <hyomee@naver.com>
2022-02-08 00:09:45 +0900

    람다

--- L/src/com/hyomee/anonymous/AnonymousAbstarctClass.java
+++ L/src/com/hyomee/anonymous/AnonymousAbstarctClass.java
@@ -4,20 +4,15 @@ class AnonymousAbstarctClass {
 
   private String workerName = "기본작업자";
 
-
-
   // 인스턴스 변수
   AnonymousAbstract workerMember = new AnonymousAbstract() {
-
     String getWorkerName() {
       return workerName;
     }
-
     @Override
     void doWork() {
       System.out.println("작업자 : " + workerName);
     }
-
     @Override
     void doWork(String str) {
         System.out.println("작업자 : " + str);
@@ -31,17 +26,14 @@ class AnonymousAbstarctClass {
       void doWork() {
         System.out.println("기본 작업자 : " + workerName + ", 작업자 : " + workerNm);
       }
-
       @Override
       void doWork(String str) {
         System.out.println("작업자 : " + str);
       }
     };
-
     worker.doWork();;
     worker.doWork("홍길동");
   }
-
 
   // 인스턴스 메서드의 파라메터
   void workerMethod(AnonymousAbstract worker) {
