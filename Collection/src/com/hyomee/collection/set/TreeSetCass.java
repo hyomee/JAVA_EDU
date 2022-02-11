e6e2aab2c22cec9b840050d3c34fc7b9a9c118f0
hyosang <hyomee@naver.com>
2022-02-11 14:28:27 +0900

    Queue

--- Collection/src/com/hyomee/collection/set/TreeSetCass.java
+++ Collection/src/com/hyomee/collection/set/TreeSetCass.java
@@ -1,17 +1,19 @@
 package com.hyomee.collection.set;
 
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 class TreeSetCass  {
   TreeSet<Integer> treeSet = new TreeSet<Integer>();
-
-
 
   TreeSetCass() {
     System.out.println("* TreeSetCass ================");
     insertTreeSet();
     getTreeSet();
     setMethod();
+    treeSet.clear();
+    sortedSet();
+
   }
 
   void insertTreeSet() {
@@ -39,5 +41,25 @@ class TreeSetCass  {
     System.out.println("* TreeSet size : " + treeSet.size());
     System.out.println("* TreeSet contains : " + treeSet.contains(8));
     System.out.println("* TreeSet contains : " + treeSet.contains(10));
+  }
+
+  void sortedSet() {
+    insertTreeSet();
+    SortedSet headSet = treeSet.headSet(5);
+    System.out.println("* TreeSet treeSet : " + treeSet.toString());
+    System.out.println("* SortedSet headSet(5) : " + headSet.toString());
+    SortedSet tailSet = treeSet.tailSet(5);
+    System.out.println("* SortedSet tailSet(5) : " + tailSet.toString());
+    SortedSet subSet = treeSet.subSet(3,6);
+    System.out.println("* SortedSet subSet(3,6) : " + subSet.toString());
+    SortedSet subSetfromInclude = treeSet.subSet(3, true, 6, false);
+    System.out.println("* SortedSet subSet(3, true, 6, false) : " + subSetfromInclude.toString());
+    SortedSet subSetToInclude = treeSet.subSet(3, false, 6, true);
+    System.out.println("* SortedSet subSet(3, false, 6, true) : " + subSetToInclude.toString());
+    SortedSet subSetFromToInclude = treeSet.subSet(3, true, 6, true);
+    System.out.println("* SortedSet subSet(3, true, 6, true) : " + subSetFromToInclude.toString());
+    SortedSet subSetFromTofalse = treeSet.subSet(3, false, 6, false);
+    System.out.println("* SortedSet subSet(3, false, 6, false) : " + subSetFromTofalse.toString());
+
   }
 }
