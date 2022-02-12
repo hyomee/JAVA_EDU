4a69215b9a8d8de6da150509d78fb9bd86f73bfe
hyosang <hyomee@naver.com>
2022-02-13 01:48:00 +0900

    상속

--- ExtendsAndImplements/src/com/hyomee/extend/Human.java
+++ ExtendsAndImplements/src/com/hyomee/extend/Human.java
@@ -2,16 +2,17 @@ package com.hyomee.extend;
 
 public class Human {
   private String name;
-  int age;
-  float weight;
-  float height;
-
   Human() { }
-
   Human(String name) {
     System.out.println("Human Overloading 생성자 생성");
     this.name = name;
   }
+
+  int age;
+  float weight;
+  float height;
+
+
 
   void setName(String name) {
     this.name = name;
