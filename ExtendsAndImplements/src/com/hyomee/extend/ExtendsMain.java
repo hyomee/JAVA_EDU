0fbced90f6e54bbf4e41383d15fd94e60adc50ed
hyosang <hyomee@naver.com>
2022-02-13 00:10:13 +0900

    상속

--- ExtendsAndImplements/src/com/hyomee/extend/ExtendsMain.java
+++ ExtendsAndImplements/src/com/hyomee/extend/ExtendsMain.java
@@ -3,7 +3,16 @@ package com.hyomee.extend;
 public class ExtendsMain {
 
   public static void main(String... args) {
+
     Student student = new Student();
+    Human human = student;
+
+    Human studentHuman = new Student();
+
+    // Student student01 = new Human();
+
+    // Student student02 = (Student) new Human();
+
 
   }
 }
