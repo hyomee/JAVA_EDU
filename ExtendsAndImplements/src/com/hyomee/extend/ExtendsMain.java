203d3c8c3862f3ccaf70af40cf829886dd0c89bc
hyosang <hyomee@naver.com>
2022-02-13 00:33:05 +0900

    상속

--- ExtendsAndImplements/src/com/hyomee/extend/ExtendsMain.java
+++ ExtendsAndImplements/src/com/hyomee/extend/ExtendsMain.java
@@ -10,12 +10,17 @@ public class ExtendsMain {
     Human studentHuman = new Student();
     Student student01 = (Student) studentHuman;
 
-    Student student02 = new Human();
+    // class com.hyomee.extend.Human cannot be cast to class com.hyomee.extend.Student
+    // Student student02 = new Human();
 
-    Student student03 = (Student) new Human();
+    // java.lang.ClassCastException: class com.hyomee.extend.Human cannot be cast to class com.hyomee.extend.Student
+    // (com.hyomee.extend.Human and com.hyomee.extend.Student are in unnamed module of loader 'app')
+    // Student student03 = (Student) new Human();
 
-    Human human01 = new Human();
-    Student student04 = (Student) human;
+    // java.lang.ClassCastException: class com.hyomee.extend.Human cannot be cast to class com.hyomee.extend.Student
+    // (com.hyomee.extend.Human and com.hyomee.extend.Student are in unnamed module of loader 'app')
+    // Human human01 = new Human();
+    // Student student04 = (Student) human01;
 
   }
 }
