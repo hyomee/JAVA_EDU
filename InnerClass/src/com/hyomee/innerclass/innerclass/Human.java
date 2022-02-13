15be7f9727e75e1078eca40a6fbc2c2d8ce1824d
hyosang <hyomee@naver.com>
2022-02-13 21:34:54 +0900

    Inner

---
+++ InnerClass/src/com/hyomee/innerclass/innerclass/Human.java
@@ -0,0 +1,30 @@
+package com.hyomee.innerclass.innerclass;
+
+public class Human {
+  String name;
+  String school;
+
+  void eat() {
+    System.out.println("사람이 밥을 먹는다.");
+  }
+
+  class Student {
+    String studentId;
+
+    Student(String studentId, String school) {
+      this.studentId = studentId;
+      Human.this.school = school;
+    }
+
+    void eat() {
+      System.out.println("* 학생 이너 클래스 *");
+      Human.this.eat();
+      System.out.println("학생이 밥을 먹는다.");
+    }
+
+    void viewStudent() {
+      System.out.println("학교 : " + Human.this.school);
+      System.out.println("학번 : " + this.studentId);
+    }
+  }
+}
--- InnerClass/src/com/hyomee/innerclass/innerclass/InnerClassMain.java
+++ InnerClass/src/com/hyomee/innerclass/innerclass/InnerClassMain.java
@@ -2,7 +2,11 @@ package com.hyomee.innerclass.innerclass;
 
 public class InnerClassMain {
   public static void main(String... args) {
-    Class aClass = new Class();
+    Human human = new Human();
+    human.eat();
+    Human.Student student = human.new Student("SB-000-0001", "한국학교");
+    student.eat();
+    student.viewStudent();
   }
 
 }
