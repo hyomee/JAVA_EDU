4ecb48536c0dbfffd85eac460ca6e95486e5a725
hyosang <hyomee@naver.com>
2022-02-14 00:19:35 +0900

    Inner

--- InnerClass/src/com/hyomee/innerclass/interfaceInner/InterfaceInnerClassMain.java
+++ InnerClass/src/com/hyomee/innerclass/interfaceInner/InterfaceInnerClassMain.java
@@ -3,7 +3,8 @@ package com.hyomee.innerclass.interfaceInner;
 public class InterfaceInnerClassMain {
   public static void main(String... args) {
     InnerfaceHuman innerfaceHuman = new InnerfaceHuman();
-    InnerfaceHuman.student student = innerfaceHuman.new StudentImpl("SB-000-0001", "한국학교");
+    InnerfaceHuman.student student =
+            innerfaceHuman.new StudentImpl("SB-000-0001", "한국학교");
     student.viewStudent();
   }
 }
