6f6fd4f58dab56817e938454e8448c6090aedfd4
hyosang <hyomee@naver.com>
2022-02-17 21:57:55 +0900

    Exception

--- Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
+++ Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
@@ -5,5 +5,9 @@ public class UnCheckedException {
   public void arithmeticExveption() {
     // Exception in thread "main" java.lang.ArithmeticException: / by zero
     int num  = 10/0;
+
+    int[] arrNum = {1,2,3};
+    //Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+    System.out.println(arrNum[3]);
   }
 }
