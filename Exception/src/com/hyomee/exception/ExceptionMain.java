d7cff4af28ed2afee33a8c4a6903f275a6b79f37
hyosang <hyomee@naver.com>
2022-02-17 21:02:57 +0900

    Exception

--- Exception/src/com/hyomee/exception/ExceptionMain.java
+++ Exception/src/com/hyomee/exception/ExceptionMain.java
@@ -1,7 +1,10 @@
 package com.hyomee.exception;
 
+import com.hyomee.exception.checkexception.UnCheckedException;
+
 public class ExceptionMain {
   public static void main(String... args) {
-
+    UnCheckedException unCheckedException = new UnCheckedException();
+    unCheckedException.arithmeticExveption();
   }
 }
---
+++ Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
@@ -0,0 +1,9 @@
+package com.hyomee.exception.checkexception;
+
+public class UnCheckedException {
+
+  public void arithmeticExveption() {
+    int num  = 10/0;
+
+  }
+}
