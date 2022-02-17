8b1cf60f2f7e534eaf48794b4f856266b1e51890
hyosang <hyomee@naver.com>
2022-02-17 22:12:17 +0900

    Exception

--- Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
+++ Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
@@ -5,14 +5,13 @@ public class UnCheckedException {
   public void arithmeticExveption() {
     try {
       int num = 10 / 0;
+      int[] arrNum = {1,2,3};
     } catch (ArithmeticException ex) {
       System.out.println("예러 발생 .... " +  ex.getMessage());
-    }
-    int[] arrNum = {1,2,3};
-    try {
-      System.out.println(arrNum[3]);
     } catch (ArrayIndexOutOfBoundsException ex) {
       System.out.println("예러 발생 .... " +  ex.getMessage());
+    } finally {
+      System.out.println("무조건 실행 ");
     }
   }
 }
