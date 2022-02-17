dfa9afc435940ee90bb84ef6d211ff5c38af4fdd
hyosang <hyomee@naver.com>
2022-02-17 21:54:04 +0900

    Exception

--- Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
+++ Exception/src/com/hyomee/exception/checkexception/UnCheckedException.java
@@ -3,6 +3,7 @@ package com.hyomee.exception.checkexception;
 public class UnCheckedException {
 
   public void arithmeticExveption() {
+    // Exception in thread "main" java.lang.ArithmeticException: / by zero
     int num  = 10/0;
   }
 }
