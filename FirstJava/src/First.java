2f0eb4a0bf647896c8923c7eaeefb9b830dd5456
hyosang <hyomee@naver.com>
2022-02-25 23:17:51 +0900

    자바 1일차 - 자바 변수

--- FirstJava/src/First.java
+++ FirstJava/src/First.java
@@ -1,11 +1,5 @@
-package com.javabasic;
-
 public class First {
     public static void main(String[] args) {
         System.out.println("콘솔에 표시 : 안녕 자바 !!!!");
-
-        int num;
-        num = 10;
-        
     }
 }
--- FirstJava/src/com/javabasic/print/SystemPrintln.java
+++ FirstJava/src/com/javabasic/print/SystemPrintln.java
@@ -2,11 +2,11 @@ package com.javabasic.print;
 
 public class SystemPrintln {
    public static void main(String[] args) {
-      System.out.println("안녕" + "자바");
-      System.out.println(3 + "자바");
-      System.out.println(3.8 + "자바");
-      System.out.println(3.8 + 2);
-      System.out.println(3.8 + 2 + "자바");
-      System.out.println("3.8" + "2");
+      System.out.println("안녕" + "자바");     // 안녕자바
+      System.out.println(3 + "자바");         // 3자바
+      System.out.println(3.8 + "자바");       // 3.8자바
+      System.out.println(3.8 + 2);           // 5.8
+      System.out.println(3.8 + 2 + "자바");   // 5.8자바
+      System.out.println("3.8" + "2");       // 3.82
    }
 }
