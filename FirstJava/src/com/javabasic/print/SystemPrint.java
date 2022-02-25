3d45912be9dc04926f78882838500d8d5cb5afc6
hyosang <hyomee@naver.com>
2022-02-25 23:27:10 +0900

    자바 1일차 - 자바 변수

--- FirstJava/src/com/javabasic/print/SystemPrint.java
+++ FirstJava/src/com/javabasic/print/SystemPrint.java
@@ -8,9 +8,9 @@ public class SystemPrint {
     public static void main(String[] args) {
         System.out.print("안녕");
         System.out.print("자바");
-        System.out.print("다음행 출력\n");
+        System.out.print("다음행 출력\n");  // 안녕자바다음행 출력
         System.out.print("2.1");
-        System.out.print("안녕");  // 안녕자바다음행 출력
+        System.out.print("안녕");          // 2.1안녕
     }
 }
 
